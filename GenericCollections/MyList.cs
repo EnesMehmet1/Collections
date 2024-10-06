2566f222a6b684e8572a58b0cfcd0b3e134a93fe
EnesMehmet1 <81644782+EnesMehmet1@users.noreply.github.com>
2024-10-06 18:45:34 +0300

    Classes Develop

---
+++ GenericCollections/MyList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    public class MyList
+    {
+        // List koleksiyon tipi IList arayüzünü implemente eder, dolayası ile Crud işlemleri ile birlikte İndexleme de vardır.
+
+        List<string> 
+
+    }
+}
---
+++ IEnumerable_ICollection_IList_IQueryable/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Queue<string> myQueue = new();
+        myQueue.Enqueue("Hello");
+        myQueue.Enqueue("Hello2");
+        myQueue.Enqueue("Hello3");
+        myQueue.Enqueue("Hello4");
+        myQueue.Enqueue("Hello5");
+        foreach (var item in myQueue)
+        {
+            Console.WriteLine(item.ToString());
+        }
+        Console.ReadLine();
+        Console.WriteLine("Beginning Item is: " + myQueue.Peek());
+
+
+
+        myQueue.Clear();
+        foreach (var item in myQueue)
+        {
+            Console.WriteLine(item);
+        }
+
+        Stack<string> stack = new Stack<string>();
+        //ArrayList
+        stack.Push("Hello");
+        stack.Push("Hello2");
+        stack.Push("Hello3");
+        stack.Push("Hello4");
+        stack.Push("Hello5");
+        stack.Push("Hello6");
+
+
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        }
+        List<string> asd = new();
+        asd.Add("Hello");
+        asd.Add("Hello");
+        asd.Add("Hello");
+        asd.Add("Hello");
+        asd.Add("Hello");
+        asd.Add("Hello");
+        var x = asd.AsQueryable();
+        foreach (var item in x)
+        {
+            Console.WriteLine(item);
+        }
+
+
+
+        var numbers = Enumerable.Range(1, 10);
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+
+
+        Queue queue = new Queue();
+        
+}
+}
